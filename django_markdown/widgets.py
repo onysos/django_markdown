6593702ce7e6d4b5b2a67e8823f304063796d8f9
darius BERNARD <darius.bernard@onysos.fr>
2013-04-11 17:49:52 +0200

    change some dummy things like add script after widget.
    used a jquery selector instead and a js extra file
    to init the marked textarea

--- django_markdown/widgets.py
+++ django_markdown/widgets.py
@@ -9,22 +9,25 @@ class MarkdownWidget(forms.Textarea):
 
     class Media:
         js = (
-            ( settings.STATIC_URL or settings.MEDIA_URL ) + 'django_markdown/jquery.markitup.js',
-            ( settings.STATIC_URL or settings.MEDIA_URL ) + 'django_markdown/markdown.js',
+            (settings.STATIC_URL or settings.MEDIA_URL) + 'django_markdown/jquery.markitup.js',
+            (settings.STATIC_URL or settings.MEDIA_URL) + 'django_markdown/markdown.js',
+            settings.STATIC_URL + "django_markdown/auto_markdown.js",
         )
         css = {
             'screen': (
-                ( settings.STATIC_URL or settings.MEDIA_URL ) + 'django_markdown/skins/%s/style.css' % getattr(settings, 'MARKDOWN_EDITOR_SKIN', 'markitup'),
-                ( settings.STATIC_URL or settings.MEDIA_URL ) + 'django_markdown/markdown.css',
+                (settings.STATIC_URL or settings.MEDIA_URL) + 'django_markdown/skins/%s/style.css' % getattr(settings, 'MARKDOWN_EDITOR_SKIN', 'markitup'),
+                (settings.STATIC_URL or settings.MEDIA_URL) + 'django_markdown/markdown.css',
             )
         }
 
     def render(self, name, value, attrs=None):
-        html = super(MarkdownWidget, self).render(name, value, attrs)
+        attrs = attrs or {}
+
+
 
         editor_settings = getattr(settings, 'MARKDOWN_EDITOR_SETTINGS', {})
         editor_settings['previewParserPath'] = reverse('django_markdown_preview')
-
-        html += '<script type="text/javascript">miu.init(\'%s\', %s)</script>' % (attrs['id'], simplejson.dumps(editor_settings))
+        attrs["data-markdown-settings"] = simplejson.dumps(editor_settings);
+        html = super(MarkdownWidget, self).render(name, value, attrs)
 
         return mark_safe(html)
